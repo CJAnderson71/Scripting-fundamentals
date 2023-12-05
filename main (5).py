40b2ec44fbec03e00905fd2cd565186ca0c83f5b
CJAnderson71 <152831653+CJAnderson71@users.noreply.github.com>
2023-12-04 20:40:58 -0500

    Lab Activity 3.8: Nested Loops

--- main (5).py
+++ main (5).py
@@ -1,18 +1,4 @@
-word = input("Word to convert: ")  # prompts user to enter word
-count = int(input("How many letters at the end of the word should be converted? "))  # prompts user to enter the count
-totalLength = len(word)  # calculates the total length of the user input word
-startLength = totalLength - count  # gets the start length which won't be in uppercase
-
-start = ""  # initialize variables
-end = ""
-
-# assigns the string from index=0 to index=startLength of variable start so that they won't be converted to uppercase
-start = word[0:startLength]
-
-# assigns the string from index=startLength to index=totalLength of variable end so that
-# they can be converted to uppercase
-end = word[startLength:totalLength]
-end = end.upper()  # convert the string in variable end to uppercase
-
-word = start + end  # appends both start and end strings
-print(word)  # prints the resultant word on the screen
+for e in range(6, 15, 2):
+  for o in range(5, 15, 2):
+    calc = e+o
+    print(e, "+", o, "=", calc)
